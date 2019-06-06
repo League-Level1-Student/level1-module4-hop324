b18bf5051e2a925abb185f2b0da68d3a00424399
League Student <league@iMac-87.attlocal.net>
2019-06-05 19:55:52 -0700

    Nasty Surprise, Simon Says
    :+1: :sunglasses: :balloon:

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -19,13 +19,18 @@ public class NastySurprise implements ActionListener{
 	JPanel panel = new JPanel();
 	
 	void createUI(){
-		frame.add(button); frame.add(button2);
+		frame.add(panel);
 		panel.add(button); panel.add(button2);
 		button.setText("Trick");
 		button.addActionListener(this);
 		button2.setText("Treat");
 		button2.addActionListener(this);
+		frame.pack();
+		frame.setVisible(true);
 		
+	}
+	public static void main(String [] args) {
+		new NastySurprise().createUI();
 	}
 	
 	private void showPictureFromTheInternet(String imageUrl) {
@@ -47,7 +52,7 @@ public class NastySurprise implements ActionListener{
 			showPictureFromTheInternet("https://hgtvhome.sndimg.com/content/dam/images/hgtv/fullset/2018/3/22/0/shutterstock_national-puppy-day-224423782.jpg.rend.hgtvcom.966.725.suffix/1521744674350.jpeg");
 		}
 		else if(e.getSource() == button2) {
-			
+			showPictureFromTheInternet("https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FAZ2ZPmEfjvU%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DAZ2ZPmEfjvU&docid=yA34_tV7kFR03M&tbnid=PTfdhkgrSBzhhM%3A&vet=10ahUKEwjTirfS29PiAhVDWqwKHTdkBLMQMwh0KAAwAA..i&w=1280&h=720&bih=879&biw=1920&q=puppy&ved=0ahUKEwjTirfS29PiAhVDWqwKHTdkBLMQMwh0KAAwAA&iact=mrc&uact=8");
 		}
 	}
 }
