27a93c91288d5a5d6381ef42441b3af05f7a67ac
League Student <league@iMac-8.attlocal.net>
2019-07-09 18:28:18 -0700

    Latest Tweet, Pig Latin Translator

---
+++ src/_09_latest_tweet/LatestTweetRunner.java
@@ -0,0 +1,9 @@
+package _09_latest_tweet;
+
+import java.awt.event.ActionListener;
+
+public class LatestTweetRunner {
+		public static void main(String [] args) {
+		new LatestTweet().createUI();
+		}
+}
---
+++ src/_10_pig_latin/PigLatinrTranslatorRunner.java
@@ -0,0 +1,7 @@
+package _10_pig_latin;
+
+public class PigLatinrTranslatorRunner {
+public static void main(String [] args) {
+	new PigLatinTranslator().createUI();
+}
+}
